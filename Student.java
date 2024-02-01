f5ae53a65f85d760ed6ccb388e268e4113c8ce46
Diksha Sharma <132244462+DikshaSharma11oct@users.noreply.github.com>
2024-02-01 18:02:27 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,7 +1,7 @@
 public class Student{
     public static void main(String[] args){
         System.out.println("Hi world");
-        System.out.println("Hi world")
+        
         // added
     }
 }
